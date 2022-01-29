68b6bb06fb46afe2f935c46735eddac4b1213f5c
cemal sezer <cemalszr35@gmail.com>
2022-01-29 03:39:00 +0300

    week-1 is done

---
+++ Week-1/StudentsManagementapi/Controller/StudentController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StudentsManagementapi.Fake;
+using StudentsManagementapi.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentsManagementapi.Controller
+{
+    [Route("api/[controller]")]
+    public class StudentController:ControllerBase
+    {
+        private List<Student> students = FakeData.GetStudents(10);
+       
+        [HttpGet]
+        public List<Student> Get()
+        {
+            return students;
+        }
+
+        [HttpGet("{id}")]
+        public Student Get(int id)
+        {
+            var student = students.FirstOrDefault(x => x.ID == id);
+            return student;
+        }
+
+        [HttpPost]
+        public Student Post([FromBody]Student student)
+        {
+            students.Add(student);
+            return student;
+        }
+
+        [HttpPut]
+        public Student Put([FromBody] Student student)
+        {
+            var editedStudent = students.FirstOrDefault(x => x.ID == student.ID);
+            editedStudent.Name = student.Name;
+            editedStudent.Surname = student.Surname;
+            return student;
+        }
+
+        [HttpDelete]
+        public void Delete(int id)
+        {
+            var deletedStudent= students.FirstOrDefault(x => x.ID == id);
+            students.Remove(deletedStudent);
+        }
+    }
+}
---
+++ Week-1/StudentsManagementapi/Fake/FakeData.cs
@@ -0,0 +1,28 @@
+﻿using StudentsManagementapi.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Bogus;
+
+namespace StudentsManagementapi.Fake
+{
+    public static class FakeData
+    {
+        private static List<Student> students;
+        public static List<Student> GetStudents(int number)
+        {
+            if (students == null)
+            {//fake kullanıcıları çekiyoruz
+                students = new Faker<Student>()
+           .RuleFor(s => s.ID, f => f.IndexFaker + 10)
+           .RuleFor(s => s.Name, f => f.Name.FirstName())
+           .RuleFor(s => s.Surname, f => f.Name.LastName())
+           .Generate(number);
+            }
+            return students;
+
+
+        }
+    }
+}
